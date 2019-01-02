035258f2ad8315ef3786aa3947bce0149fc0ba95
Joe-ayy <jwharrison96@gmail.com>
2019-01-02 17:18:08 -0500

    Add files via upload
    Latest and greatest version so far

---
+++ sharpclean/sharpclean/home.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+namespace sharpclean
+{
+    class main
+    {
+        static int Main(string[] args)
+        {
+            home m_h = new home();
+            m_h.run();
+
+            return 0;
+        }
+    }
+
+    class home
+    {
+        //enter here
+        public void run()
+        {
+            while (good)
+            {
+                if (img == null)
+                    startmenu();
+                else
+                    fullmenu();
+            }
+
+            deleteimage();
+        }
+
+        //initial menu when no image is loaded
+        private void startmenu()
+        {
+            int n = 0;
+
+            if (cmd.getcmd("[1]load - ", ref n, 0))
+            {
+                if (n == 1)
+                    loadfile();
+                else
+                    System.Console.WriteLine(home_err + "invalid command\n");
+            }       
+            else
+                good = false;
+        }
+
+        //menu with all options, only when image is loaded
+        private void fullmenu()
+        {
+            int n = 0;
+
+            if (cmd.getcmd("[1]load, [2]save, [3]print menu, [4]clean [q]quit - ", ref n, 0))
+            {
+                switch (n)
+                {
+                    case 1: loadfile(); break;
+                    case 2: writefile(); break;
+                    case 3:
+                        {
+                            if (img != null)
+                                img.printmenu();
+                            else
+                                System.Console.WriteLine(home_err + "image not loaded\n");
+                            break;
+                        }
+                    case 4:
+                        {
+                            if (t != null)
+                                t.clean();
+                            else
+                                System.Console.WriteLine(home_err + "toolbox not loaded\n");
+                            break;
+                        }
+                }
+            }
+            else good = false;
+        }
+
+        //loads a pgm
+        private void loadfile()
+        {
+            string f = "";
+            int n = 0;
+
+            if (img != null)
+            {
+                if (cmd.getcmd("delete loaded image? [1]yes, [q]quit - ", ref n, 1))
+                    deleteimage();
+                else return;
+            }
+
+            if (cmd.getfile("enter file name : ", ref f, ".pgm", 1))
+            {
+                f = "images/" + f;
+                img = new image();
+                if (img.load(f))
+                    t = new toolbox(img.getpixels(), img.getImageData().width, img.getImageData().totalpixels);
+                else
+                    deleteimage();
+            }
+        }
+
+        //writes to a pgm
+        private void writefile()
+        {
+            if (img != null)
+            {
+                string f = "";
+
+                if (cmd.getfile("enter file name : ", ref f, ".pgm", 1))
+                {
+                    f = "saves/" + f;
+                    System.Console.WriteLine(f);
+                    img.write(f);
+                }
+            }
+            else System.Console.WriteLine(home_err + "no image loaded to save");
+        }
+
+        //if toolbox is set up give it up then give up the loaded image
+        private void deleteimage()
+        {
+            if (t != null)
+                t = null;
+
+            img = null;
+        }
+
+        private bool good = true;
+        private command cmd = new command();
+        toolbox t = null;
+        image img = null;
+        private readonly string home_err = "::HOME::error : ";
+    }
+}
+*/
---
+++ sharpclean/sharpclean/selection.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharpclean
+{
+    class selection
+    {
+        public const byte VALUE_THRESHOLD = 255;
+        selection()
+        {
+            Console.Write(selection_err + "initialized without pixels\n");
+        }
+
+        public selection(pixel[] p, int width, int total)
+        {
+            buffer = new List<int>();
+            perimeter = new List<int>();
+            pixels = p;
+            this.width = width;
+            this.total = total;
+            bufferSize = 0;
+        }
+
+        public bool get(int i)
+        {
+	        if (checkPixel(ref pixels[i], VALUE_THRESHOLD))
+	        {
+		        iterate(VALUE_THRESHOLD);
+
+		        if (bufferSize > 0 && bufferSize< 2700)
+		        {
+			        fillPixels();
+                    findEdges();
+			        return true;
+		        }
+            }
+	        return false;
+        }
+
+
+
+        private void iterate(int value)
+        {
+            for (int i = 0; i < bufferSize - 1; i++)
+                nextPixel(buffer[i], value);
+        }
+
+        private void nextPixel(long i, int value)
+        {
+            if ((i - width - 1) > 0)
+            {
+                checkPixel(ref pixels[i - width - 1], value);   //top left
+                checkPixel(ref pixels[i - width], value);       //top center
+                checkPixel(ref pixels[i - width + 1], value);   //top right
+            }
+
+            if (i % width != 0)
+                checkPixel(ref pixels[i - 1], value);   //center left
+            if (i % (width + 1) != 0)
+                checkPixel(ref pixels[i + 1], value);   //center right
+
+            if ((i + width + 1) < total)
+            {
+                checkPixel(ref pixels[i + width - 1], value);   //bottom left
+                checkPixel(ref pixels[i + width], value);       //bottom center
+                checkPixel(ref pixels[i + width + 1], value);   //bottom right
+            }
+        }
+
+        private bool checkPixel(ref pixel p, int value)
+        {
+            if (p.value < value)
+            {
+                if (!p.selected)
+                {
+                    buffer.Add(p.id);
+                    p.selected = true;
+                    bufferSize++;
+                    tree.insert(buff, p.id);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void fillPixels()
+        {
+            filler fill = new filler(pixels, width, total);
+            for (int i = 0; i < bufferSize; i++)
+                fill.getBounds(buffer[i]);
+            int count = 0;
+            for (int i = 0; i < bufferSize; i++)
+            {
+                if (buffer[i] + width < total && !pixels[buffer[i] + width].selected && pixels[buffer[i] + width].value >= VALUE_THRESHOLD)
+                {
+                    fill.start(buffer[i] + width);
+                    List<pathDirection> whitePixels = fill.getPath();
+                    if (whitePixels.Count != 0)
+                    {
+                        //j once was i, not sure how this wasn't an issue in c++
+                        //but reassigning i here may have slowed things down a lot
+
+                        for (int j = 0; j < whitePixels.Count; j++)
+                        {
+                            buffer.Add(whitePixels[j].id);
+                            tree.insert(buff, whitePixels[j].id);
+                            count++;
+                        }
+                    }
+                }
+            }
+            fill.clearFoundBuffer();
+            bufferSize += count;
+        }
+
+        private void findEdges()
+        {
+            edge e = new edge(width, total);
+            e.detect(buffer, buff);
+            perimeter = e.getPerimiter();
+            numEdges = e.getEdges();
+        }
+
+        public ref List<int> getBuffer()
+        {
+	        return ref buffer;
+        }
+
+        public ref List<int>  getPerimeter()
+        {
+	        return ref perimeter;
+        }
+
+        public int getEdges()
+        {
+            return numEdges;
+        }
+
+        public void clearBuffer()
+        {
+            buffer.Clear();
+            bufferSize = 0;
+            buff = null;
+        }
+
+        private pixel[] pixels;
+        private int width, total;
+        private List<int> buffer, perimeter;
+        private int bufferSize, numEdges;
+        private node buff;
+        private readonly string selection_err = "::SELECTION::error : ";
+    }
+}
