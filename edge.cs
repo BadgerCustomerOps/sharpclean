13970d6871572ab27dffb9b547208b41805a7ff8
BadgerCustomerOps <46000890+BadgerCustomerOps@users.noreply.github.com>
2019-01-03 12:19:46 -0500

    added comments to edge

--- edge.cs
+++ edge.cs
@@ -25,15 +25,16 @@ namespace sharpclean
             total = t;
         }
 
+        // takes the full selected object and the root node of the binary tree buffer
         public void detect(List<int> mselection, node buff)
         {
             sel = buff;
-            stack.Add(mselection[0]);
+            stack.Add(mselection[0]); // the first pixel is always an edge, add it to stack and perimeter/tree
             perimeter.Add(mselection[0]);
             tree.insert(ref per, mselection[0]);
             perimSize++;
             numEdges++;
-            iterateEdges();
+            iterateEdges(); // begin iterating to find edges
 
             per = null;
             sel = null;
@@ -41,37 +42,40 @@ namespace sharpclean
 
         private void iterateEdges()
         {
-            while (stack.Count != 0)
+            while (stack.Count != 0) // while the stack isn't empty, pop the top pixel off and use it to find the next edge pixel if one exists
             {
-                int id = stack[stack.Count - 1];
+                int id = stack[stack.Count - 1]; // pop
                 stack.RemoveAt(stack.Count - 1);
-                getOctan(id);
+                getOctan(id); // get this pixel's octan
             }
         }
 
         private void getOctan(int id)
         {
+            // create an octan and populate it with pixels, a pixel id of -1 means it doesn't exist in the selection (not necessarily in the pixel array)
             octan oct = new octan();
-            if ((id - width) > 0)
+
+            if ((id - width) > 0) // check top left/center/right
             {
                 oct.tl = tree.findNode(sel, id - width - 1);
                 oct.t = tree.findNode(sel, id - width);
                 oct.tr = tree.findNode(sel, id - width + 1);
             }
 
-            if (id % width != 0)
+            if (id % width != 0) // check left
                 oct.l = tree.findNode(sel, id - 1);
 
-            if ((id + 1) % width != 0)
+            if ((id + 1) % width != 0) // check right
                 oct.r = tree.findNode(sel, id + 1);
 
-            if ((id + width) < total)
+            if ((id + width) < total) // check bottom left/center/right
             {
                 oct.bl = tree.findNode(sel, id + width - 1);
                 oct.b = tree.findNode(sel, id + width);
                 oct.br = tree.findNode(sel, id + width + 1);
             }
 
+            // now see if the octan contains an edge pixel, same order as above
             if ((id - width) > 0)
             { //read: "check if top left is an edge"
                 check(oct.tl, oct.t, oct.l, n.tl);
@@ -95,23 +99,22 @@ namespace sharpclean
 
         private void check(int p, int p1, int p2, int mneighbor)
         {
-            if (p != -1 && !(p1 != -1 && p2 != -1))
+            if (p != -1 && !(p1 != -1 && p2 != -1)) // if p exists and p1 or p2 does not exist
             {
-                if (tree.insert(ref per, p))
+                if (tree.insert(ref per, p)) // insert it as a perimeter pixel, add to stack to find more, set field and run a tolerance check for edge compression
                 {
                     stack.Add(p);
                     perimeter.Add(p);
                     perimSize++;
 
-                    if (!fieldSet)
-                    {
+                    if (!fieldSet) {
                         setField(mneighbor);
                         numEdges++;
                     }
                     else
                     {
                         tolerance += field[mneighbor];
-                        if (tolerance < -4 || tolerance > 4)
+                        if (tolerance < -4 || tolerance > 4) //if a perimeter pixel is too far from the original line then we want to consider it the start of a new edge
                         {
                             tolerance = 0;
                             numEdges++;
@@ -123,7 +126,7 @@ namespace sharpclean
         }
 
         private void setField(int mneighbor)
-        {
+        {   // a field assigns values for the tolerance check
             if (mneighbor == n.t || mneighbor == n.b)
             {   //vertical
                 field[n.tl] = -1;
@@ -190,10 +193,10 @@ namespace sharpclean
             return numEdges;
         }
 
-        private node sel = new node();
-        private node per = new node();
-        private node edg = new node();
-        private int[] field = new int[8];
+        private node sel = new node(); // selection tree
+        private node per = new node(); // perimeter tree
+        private node edg = new node(); // edge tree
+        private int[] field = new int[8]; // a field that will have set values for compressing perimeter pixels into edges
         private bool fieldSet;
         private List<int> perimeter = new List<int>();
         private List<int> stack = new List<int>();
